fc3d8dbedceb74d6574524116dcf5cfcb551f147
Maksim Shelest <shelest.ks@gmail.com>
2022-07-08 16:27:35 +0300

    добавил авторов

--- src/components/BookBlock/BookBlock.tsx
+++ src/components/BookBlock/BookBlock.tsx
@@ -3,6 +3,8 @@ import { TVolumeInfo } from '../../redux/searchedBooks/types';
 import Button from '../UI/Button/Button';
 import s from './BookBlock.module.scss';
 
+const noCoverImage = 'https://books.google.ru/googlebooks/images/no_cover_thumb.gif';
+
 type BookBlockProps = { volumeInfo: TVolumeInfo };
 
 const BookBlock: React.FC<BookBlockProps> = ({ volumeInfo }) => {
@@ -24,11 +26,12 @@ const BookBlock: React.FC<BookBlockProps> = ({ volumeInfo }) => {
   return (
     <div className={s.item}>
       <div className={s.image}>
-        <img src={imageLinks?.thumbnail} alt="" />
-      </div>
+        <img src={imageLinks ? imageLinks?.thumbnail : noCoverImage} alt="" />
+		  </div>
+		  <div className={s.authors}>{authors?.join(" ")}</div>
       <div className={s.title}>{title}</div>
       {/* <div className={s.subtitle}>{subtitle}</div> */}
-		  <Button style ={{justifySelf:"center"}}>В избранное</Button>
+      <Button style={{ justifySelf: 'center' }}>В избранное</Button>
     </div>
   );
 };
