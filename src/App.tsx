eecd9ac5d561de0eecf51b341b7e02dadb44475e
Maksim Shelest <shelest.ks@gmail.com>
2022-07-11 15:32:48 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Home from './pages/Home/Home';
 import './scss/app.scss';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
+import Favorite from './pages/Favorite/Favorite';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path='' element={<Home />} />
+        <Route path="" element={<Home />} />
+        <Route path="/favorite" element={<Favorite />} />
       </Route>
     </Routes>
   );
