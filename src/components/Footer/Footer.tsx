377ee64ea76a302baa7a74fa8ccb134ec3dae5f7
Maksim Shelest <shelest.ks@gmail.com>
2022-07-18 18:09:03 +0300

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Footer.module.scss'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+	 <footer className={s.footer}>Footer</footer>
+  )
+}
+
+export default Footer
