e93f7cdccf23b6b514ea546431f70cf4ef722c70
Maksim Shelest <shelest.ks@gmail.com>
2022-07-26 13:18:21 +0300

    chunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import PageHome from './pages/PageHome/PageHome';
+import React, { lazy, Suspense } from 'react';
 import './scss/app.scss';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import PageFavorite from './pages/PageFavorite/PageFavorite';
-import PageBook from './pages/PageBook/PageBook';
-import PageNotFound from './pages/PageNotFound/PageNotFound';
+import PageHome from './pages/PageHome/PageHome';
+import Loading from './components/UI/Loading/Loading';
+
+const PageFavorite = lazy(() => import('./pages/PageFavorite/PageFavorite'));
+const PageBook = lazy(() => import('./pages/PageBook/PageBook'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound/PageNotFound'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<PageHome />} />
-        <Route path="/favorite" element={<PageFavorite />} />
-        <Route path="/book/:id" element={<PageBook />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<PageHome />} />
+          <Route path="/favorite" element={<PageFavorite />} />
+          <Route path="/book/:id" element={<PageBook />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
